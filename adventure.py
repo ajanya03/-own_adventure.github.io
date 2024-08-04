68ffa258e404790a60e190efee2e0d7e17d47e99
ajanya03 <130043690+ajanya03@users.noreply.github.com>
2024-08-04 11:58:07 +0530

    Update adventure.py

---
+++ adventure.py
@@ -0,0 +1,96 @@
+def main():
+    user_input = int(input("Enter 1 to play , 0 to quit : "))
+    if user_input == 1:
+        print("Let's begin our adventure!!!")
+        head_start()
+    elif user_input == 0:
+        print("We are sorry to see you leave!!!")
+    else:
+        print("Invalid Input")      
+    
+
+def head_start():
+    print("You are on a palace and you want to escape from it.Suddenly it's getting dark and you want to see the way out .")
+    command_1 = input("There is a lamp and a torch is lying near by.What do you want to pick ? Torch or Lamp : ").lower()
+    if command_1 == 'torch':
+        print("You picked the torch.")
+        with_torch()
+    elif command_1 == 'lamp':
+        print("You picked the lamp.")
+        with_lamp()
+    else:
+        print("Invalid Input")  
+
+# journey using torch
+
+def with_torch():
+    print("Now you entered a big garden full of roses.")
+    print("Do you want to smell it or go straight?")
+    command_2 = input("Type smell / straight : ").lower()
+    if command_2 == 'smell':
+        print("You smelled the rose, unfortunately it's poisonous rose.")
+        print("You are now dead!!!")
+    elif command_2 == 'straight':
+        print("You decided to go straight ahead")
+        go_straight()
+    else:
+        print("Invalid Input")  
+
+def go_straight():
+    print("Your torch is not working but still you continued your journey without knowing any path.Suddenly one dragon entered and coming towards you . You can either run or accept your defeat.Choose wisely")
+    command_3 = input("Choose between run / accept : ").lower()
+    if command_3 == 'run':
+        print("You run into a wall, hit and died!!!")
+        print("Told you to choose wisely!!!")
+        print("The End")
+        
+    elif command_3 == 'accept':
+        print("No need to panic , it's just a ballon dragon.You are brave to accept your defeat infront of dragon.Impressive!!!")
+        print("You won this adventure!!!HURRAY!!!")
+    else:
+        print("Invalid Input")  
+        
+
+# journey using lamp
+
+def with_lamp():
+    print("You entered a room full of perfumes.Do you want to smell it for long or put a mask on ?")
+    command_4 = input("Choose between smell / mask : ").lower()
+    if command_4 == 'smell':
+        print("Your bad....It's poisoned.You died!!!")
+        
+    elif command_4 == 'mask':
+        print("You put a mask on.")
+        with_mask()
+    else:
+        print("Invalid Input")  
+
+def with_mask():
+    print("Now you are going straight ahead.Look there's little wind blowing towards you . Unfortunetly it blow out your lamp.Now you need to decide either stay right there or go ahead into the darkness.")
+    command_5 = input("Choose between stay / continue : ").lower()
+    if command_5 == 'stay':
+        print("You choosed to stay.")
+        by_staying()
+    elif command_5 == 'continue':
+        print("You walk into a hole.There is a demon inside .")
+        print("Unfortunately you died!!!")
+        print("The End")
+    else:
+        print("Invalid Input")  
+        
+
+def by_staying():
+    print(" Staying in the dark made you fall asleep.Suddenly a sound echoed from a distance.\'YOU ARE NOW THE CHOSEN ONE.GET UP LITTLE KID\' .and it disppear into thin air. You can either follow the voice or do nothing. ")
+    command_6 = input("Choose between follow / stay : ").lower()
+    if command_6 == 'stay':
+        print("By staying there for the whole night you wake up in the morning and begin your journey by following the light ahead.You found the way out . HURRAY!!!")
+        print("You won!!!")
+        
+    elif command_6 == 'follow':
+        print("You follow the voice and tripped over into a hole.Unfortunately you DIED!!!!")
+        print("The End")
+    else:
+        print("Invalid Input")           
+
+if __name__ == "__main__":
+    main()
